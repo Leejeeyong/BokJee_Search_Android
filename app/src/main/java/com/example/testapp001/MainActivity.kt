7cc518710fc1498fb083d6a709e16f8e8c9ab110
JeeYong LEE <fghj0720@gmail.com>
2021-04-05 10:29:48 +0900

    Webview 화면 기능 구현\

--- app/src/main/java/com/example/testapp001/MainActivity.kt
+++ app/src/main/java/com/example/testapp001/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.testapp001
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
+
+        val homeButton = findViewById<Button>(R.id.button_home)
+
+
+        homeButton.setOnClickListener {
+            val intent = Intent(this, WebView::class.java)
+
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/testapp001/WebView.kt
+++ app/src/main/java/com/example/testapp001/WebView.kt
@@ -11,7 +11,7 @@ import android.webkit.WebViewClient
 
 class WebView : AppCompatActivity() {
 
-    private val url = "https://naracloud.com"
+    private val url = "https://google.com"
     private val TAG = "MainWebView"
 
     val Base_URL = "https://wuqhn9t75a.execute-api.ap-northeast-2.amazonaws.com/"
